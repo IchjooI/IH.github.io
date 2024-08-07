5e89c816ca379bfe0f1bffa2d40303f858d08b98
IchjooI <106009887+IchjooI@users.noreply.github.com>
2024-08-07 03:55:47 +0000

    Deploying to gh-pages from @ IchjooI/IH-Code@e5a529e1cffe2acafdd2119a506856540e1eec64 🚀

---
+++ js/about.956bcab1.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkih_code"]=self["webpackChunkih_code"]||[]).push([[594],{603:function(n,u,e){e.r(u),e.d(u,{default:function(){return d}});var t=e(6768);const a={class:"about"},c=(0,t.Lk)("h1",null,"This is an about page",-1),r=[c];function s(n,u){return(0,t.uX)(),(0,t.CE)("div",a,r)}var i=e(1241);const o={},h=(0,i.A)(o,[["render",s]]);var d=h}}]);
+//# sourceMappingURL=about.956bcab1.js.map
